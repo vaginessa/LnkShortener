da95937376dd966855e7ac7df9a7873b67000d19
Michael Achmann <michael.achmann@gmail.com>
2017-04-16 11:30:11 +0200

    Further changes. Started Adding Setup Assitant including shiny colors

---
+++ app/src/main/java/de/hirtenstrasse/michael/lnkshortener/SetupStep3LoginFragment.java
@@ -0,0 +1,29 @@
+package de.hirtenstrasse.michael.lnkshortener;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SetupStep3LoginFragment extends Fragment {
+
+
+    public SetupStep3LoginFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_setup_step3_login, container, false);
+    }
+
+}
